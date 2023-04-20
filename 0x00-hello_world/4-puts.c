eaeef1c36c88c46ba8a66b9b882bd5e603f32008
malakzaidi <malakzaidi815@gmail.com>
2023-04-20 16:58:42 -0700

    hj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
+
+#include<stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     *//
 
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
 int main(void)
 
 {
