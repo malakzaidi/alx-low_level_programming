218e2f3ea8050965b82d8c06e3ed3856719cde5b
malakzaidi <malakzaidi815@gmail.com>
2023-04-20 16:11:56 -0700

    mandatory projects of C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,28 @@
+
 #include <stdio.h>
 
 /**
- * main - Entry point
+
+ * main - 4-puts.c
+
+ * description - Write a C program that prints exactly
+
  *
+
  * Return: Always 0 (Success)
+
  */
+
 int main(void)
+
 {
+
         puts("\"Programming is like building a multilingual puzzle");
+
+
         return (0);
+
 }
+
+
+
