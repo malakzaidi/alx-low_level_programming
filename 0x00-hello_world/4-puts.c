2b2a946be5e59589247046cd68bb85caebf7ba01
malakzaidi <malakzaidi815@gmail.com>
2023-04-20 17:10:27 -0700

    hj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: Prints the message "Programming is like building a multilingual puzzle"
+ * Description: Prints a string using puts
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
 
-        return (0);
+    return (0);
 }
