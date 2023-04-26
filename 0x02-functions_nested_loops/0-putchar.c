c56afb2bb365a4e89c732d2dd1271dae927c57b3
malakzaidi <malakzaidi815@gmail.com>
2023-04-26 15:13:41 -0700

    kjl

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,18 @@
 #include "main.h"
+
+/*
+ * This is a C program that prints out the string "Holberton"
+ * using ASCII codes. It includes the standard header file "main.h"
+ * and defines the main function, which returns an integer value of 0
+ * to indicate successful execution.
+ */
 /**
- * main - Prints Holberton as a message.
- *
- * Return: Always 0 (Success)
- */
+ * main - Entry point of the program
+ *
+ * Description: Prints out the string "Holberton" using ASCII codes
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
