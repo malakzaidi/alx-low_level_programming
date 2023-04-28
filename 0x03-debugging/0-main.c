20ebcb69bf4a76496019a18e9965746e55b8d959
malakzaidi <malakzaidi815@gmail.com>
2023-04-28 15:47:01 -0700

    w

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
-* main - Tests if a number is positive or negative
-*
-* Return: Always 0 (Success)
-*/
+ * main - Test function for positive or negative
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
 return (0);
 }
+
