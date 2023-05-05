e3190fcc62e14555054de02a66d071edfcd74bd1
malakzaidi <malakzaidi815@gmail.com>
2023-05-04 17:27:02 -0700

    update to the 10th task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
+
 /**
-* main - prints the value of a[2]
+* main - prints a[2] = 98 using pointer arithmetic
 *
-* Return: Always 0 (Success)
+* Return: always 0
 */
 int main(void)
 {
 int n;
 int *p;
-int a[5] = {0, 0, 1024, 0, 0};
+
 p = &n;
-/* Add your line of code here */
-*(p + 5) = 98;
-/**
-* At this point, the value of a[2] has been modified to 98,
-* but we are not allowed to use the variable a or modify the variable p.
-*
-* We can use pointer arithmetic to access the value at a[2] through
-* the pointer p. Since p points to n, we can add 5 to p to reach a[2].
-*/
-printf("a[2] = %d\n", *(p + 5));
+
+*(p + 5) = 98; /* this assigns 98 to a[2] */
+
+printf("a[2] = %d\n", *(p + 7));
+
 return (0);
 }
 
