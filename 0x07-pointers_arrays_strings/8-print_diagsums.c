d3a4dc628dcf5796440724d642cf1a4d21b62912
malakzaidi <malakzaidi815@gmail.com>
2023-05-09 16:45:55 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 #include <stdio.h>
 /**
@@ -20,6 +19,7 @@ for (y = size - 1; y >= 0; y--)
 {
 sum2 += a[y * size + (size - y - 1)];
 }
-printf("%d, %d\n", sum1, su
+printf("%d, %d\n", sum1, sum2);
+}
 
 
